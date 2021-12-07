f2efd9a89fb9950e5fda1cda8f36031a98feb630
Laurent Meunier <laurent@deltalima.net>
2021-12-07 19:14:31 +0100

    2021 day 07

---
+++ 2021/day07/part2.py
@@ -0,0 +1,17 @@
+with open("input", "r") as f:
+    lines = f.readlines()
+
+positions = list(map(int, lines[0].strip().split(",")))
+
+position_min = min(positions)
+position_max = max(positions)
+
+best_fuel_qty = None
+for position in range(position_min, position_max + 1):
+    fuel_qty = sum(map(lambda x: int(((x-position)**2 + abs(x-position))/2), positions))
+    if best_fuel_qty is None:
+        best_fuel_qty = fuel_qty
+    elif fuel_qty < best_fuel_qty:
+        best_fuel_qty = fuel_qty
+
+print(best_fuel_qty)
